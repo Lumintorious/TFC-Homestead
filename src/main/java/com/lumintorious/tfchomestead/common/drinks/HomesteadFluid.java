2bc5412e98072db2f48257f9133fe387bfcda460
pehala <halapetr@selfnet.cz>
2022-10-16 22:15:34 +0200

    Optimize AGED_ALCOHOL map
    - Rewrite streams using an entryset which can do the same things in only one loop, instead of two
    - Remove use of BiMap as it is no longer needed

--- src/main/java/com/lumintorious/tfchomestead/common/drinks/HomesteadFluid.java
+++ src/main/java/com/lumintorious/tfchomestead/common/drinks/HomesteadFluid.java
@@ -1,14 +1,13 @@
 package com.lumintorious.tfchomestead.common.drinks;
 
-import com.google.common.collect.BiMap;
-import com.google.common.collect.HashBiMap;
 import com.lumintorious.tfchomestead.TFCHomestead;
 import net.dries007.tfc.common.blocks.TFCBlocks;
-import net.dries007.tfc.common.fluids.*;
+import net.dries007.tfc.common.fluids.FlowingFluidRegistryObject;
+import net.dries007.tfc.common.fluids.FluidType;
+import net.dries007.tfc.common.fluids.MixingFluid;
 import net.dries007.tfc.common.items.TFCItems;
 import net.dries007.tfc.util.Helpers;
 import net.dries007.tfc.util.registry.RegistrationHelpers;
-import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.level.material.FlowingFluid;
 import net.minecraft.world.level.material.Fluid;
@@ -29,13 +28,12 @@ import static net.dries007.tfc.common.fluids.TFCFluids.*;
 public class HomesteadFluid {
     public static final DeferredRegister<Fluid> FLUIDS = DeferredRegister.create(ForgeRegistries.FLUIDS, TFCHomestead.MOD_ID);;
 
-    public static final BiMap<AgedAlcohol, FlowingFluidRegistryObject<MixingFluid>> AGED_ALCOHOL;
+    public static final Map<AgedAlcohol, FlowingFluidRegistryObject<MixingFluid>> AGED_ALCOHOL;
 
     static {
-        var oneWayMap = Helpers.mapOfKeys(AgedAlcohol.class, (fluid) -> register(fluid.getId(), "flowing_" + fluid.getId(), (properties) -> {
+        AGED_ALCOHOL = Helpers.mapOfKeys(AgedAlcohol.class, (fluid) -> register(fluid.getId(), "flowing_" + fluid.getId(), (properties) -> {
             properties.block(TFCBlocks.ALCOHOLS.get(fluid)).bucket(TFCItems.FLUID_BUCKETS.get(FluidType.asType(fluid)));
         }, FluidAttributes.builder(WATER_STILL, WATER_FLOW).translationKey("fluid.tfchomestead." + fluid.getId()).color(fluid.getColor()).overlay(WATER_OVERLAY).sound(SoundEvents.BUCKET_FILL, SoundEvents.BUCKET_EMPTY), MixingFluid.Source::new, MixingFluid.Flowing::new));
-        AGED_ALCOHOL = HashBiMap.create(oneWayMap);
     }
 
     private static <F extends FlowingFluid> FlowingFluidRegistryObject<F> register(String sourceName, String flowingName, Consumer<ForgeFlowingFluid.Properties> builder, FluidAttributes.Builder attributes, Function<ForgeFlowingFluid.Properties, F> sourceFactory, Function<ForgeFlowingFluid.Properties, F> flowingFactory) {
